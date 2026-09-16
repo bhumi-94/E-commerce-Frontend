2f69032045e60e8e93d5ee3dbba6bf62beae961a
Bhoomi Kaushik <kaushikbhumika13@gmail.com>
2026-09-16 12:26:21 +0530

    Update Loading.jsx and Profile.jsx

--- src/Components/common/Loading.jsx
+++ src/Components/common/Loading.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <svg className="h-5 w-5 animate-spin" viewBox="0 0 24 24" fill="none">
+    <div className="flex justify-center items-center">
+      <svg className="h-5 w-5 animate-spin" viewBox="0 0 24 24" fill="none">
       <circle
         cx="12"
         cy="12"
@@ -19,6 +20,7 @@ const Loading = () => {
         strokeLinecap="round"
       />
     </svg>
+    </div>
   );
 };
 
